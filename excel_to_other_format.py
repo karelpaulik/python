10ee5337cb3a556c480c992d771a6974e70454e9
karelpaulik <60463516+karelpaulik@users.noreply.github.com>
2025-08-12 16:05:27 +0200

    Add files via upload

---
+++ excel_to_other_format.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+"""
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data)
+df.to_excel("new_file.xlsx")
+"""
+
+df = pd.read_excel("calc.xlsx", sheet_name="detail_cz")
+print(df.to_string())
+
+df.to_json("new_file.json")
+df.to_csv("new_file.csv")
+df.to_excel("new_file.xlsx")
+
