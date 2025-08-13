34a172e94466d494b3533e915f8b24cd211b2f94
karelpaulik <60463516+karelpaulik@users.noreply.github.com>
2025-08-13 09:23:00 +0200

    Update excel_to_other_format.py

--- excel_to_other_format.py
+++ excel_to_other_format.py
@@ -11,10 +11,9 @@ df.to_excel("new_file.xlsx")
 """
 
 df = pd.read_excel("calc.xlsx", sheet_name="detail_cz")
-print(df.to_string())
+print(df.to_string())  # výpis celé tabulky
+print(df)              # stručný výpis. Tj. U velké tabulky zobrazí pár prvních a posledních řádků.
 
 df.to_json("new_file.json", orient='records')  # orient='records': je důležitý pro správnou strukturu json file. orient můžu nabývat: {‘split’, ‘records’, ‘index’, ‘columns’, ‘values’, ‘table’}.
 df.to_csv("new_file.csv")
 df.to_excel("new_file.xlsx")
-
-
