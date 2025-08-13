c50532c23022a9cb86c068c7bc6db81be7d8ff66
karelpaulik <60463516+karelpaulik@users.noreply.github.com>
2025-08-13 09:01:39 +0200

    Update excel_to_other_format.py

--- excel_to_other_format.py
+++ excel_to_other_format.py
@@ -13,7 +13,8 @@ df.to_excel("new_file.xlsx")
 df = pd.read_excel("calc.xlsx", sheet_name="detail_cz")
 print(df.to_string())
 
-df.to_json("new_file.json")
+df.to_json("new_file.json", orient='records')  # orient='records': je důležitý pro správnou strukturu json file. orient můžu nabývat: {‘split’, ‘records’, ‘index’, ‘columns’, ‘values’, ‘table’}.
 df.to_csv("new_file.csv")
 df.to_excel("new_file.xlsx")
+
 
